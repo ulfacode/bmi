dc72c685b2ef01154174a66c9aa97016b2787972
Ulfatun Nasikhah <ulfatunnasikhah49@gmail.com>
2022-07-05 07:26:43 +0700

    3. BMI - Kode Halaman Dasar dan Layout

--- lib/bmi_result_screen.dart
+++ lib/bmi_result_screen.dart
@@ -1,16 +1,14 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class BmiResultScreen extends StatelessWidget {
+  const BmiResultScreen({Key? key}) : super(key: key);
 
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hasil Hitung BMI"),
+      ),
+    );
   }
 }
