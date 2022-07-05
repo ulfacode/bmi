aa80a5b42fd04ff6631f109f163523625e835288
Ulfatun Nasikhah <ulfatunnasikhah49@gmail.com>
2022-07-05 18:34:06 +0700

    5. BMI - UI Card Gender

--- lib/bmi_data_screen.dart
+++ lib/bmi_data_screen.dart
@@ -18,9 +18,54 @@ class _BmiDataScreenState extends State<BmiDataScreen> {
       body: Column(
         children: [
           Expanded(
-              child: Container(
-            color: Colors.red,
-          )),
+            child: Container(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.male,
+                            size: 80,
+                          ),
+                          Text(
+                            "Male",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff8d8e98),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.female,
+                          size: 80,
+                        ),
+                        Text(
+                          "Female",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff8d8e98),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           Expanded(
               child: Container(
             color: Colors.yellow,
